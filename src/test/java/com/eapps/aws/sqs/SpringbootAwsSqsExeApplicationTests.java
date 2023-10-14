467f8659e982bb3a4a5c41076f5469d28184bf1e
udit <uditkumar063@gmail.com>
2023-10-15 01:55:44 +0530

    committed for setup aws sqs repo

---
+++ src/test/java/com/eapps/aws/sqs/SpringbootAwsSqsExeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eapps.aws.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsSqsExeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
